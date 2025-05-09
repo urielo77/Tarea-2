2242610ccf1b5a07632dcf93b7793c365b5dac73
urielo77 <uruiz7794@gmail.com>
2025-05-08 22:46:12 -0600

    Create Ejercicio12.c

---
+++ Ejercicio12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+    {
+        int N, i;
+        producto = 1;
+
+        for (i = 1; i <= N; i++) 
+            {
+                prod = prod*i;
+            }
+    
+        printf("El producto de los primeros %d números naturales es: %llu\n", N, producto);
+    
+        return 0;
+    }
